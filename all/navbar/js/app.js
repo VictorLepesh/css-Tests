58b22e40a1c4ad9e16a40129d6fe24224e97dfe3
Vic <victorl175@protonmail.com>
2019-09-30 10:03:13 -0700

    Updating scores, practiced with CSS, no new high scores.  Almost got 20 points in appRace from the functino, just had a small typo

--- all/navbar/js/app.js
+++ all/navbar/js/app.js
@@ -3,29 +3,18 @@ const navSlide = () => {
     const nav = document.querySelector(".nav-links");
     const navLinks = document.querySelectorAll(".nav-links li");
 
-    // Toggle Nav
-    burgerIcon.addEventListener("click", () => {
+    burgerIcon.addEventListener("click", () => { // Toggle Nav
         nav.classList.toggle("nav-active");
 
-        // Animate links
-        navLinks.forEach((link, index) => {
-            // Use backticks for the string to make it work.
+        navLinks.forEach((link, index) => { // Animate links
             if (link.style.animation) {
                 link.style.animation = "";
             } else {
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 1.5}s`;
-            }
-            console.log(link.style.animation);
-
-            //`navLinkFade 0.5s ease forwards ${index / 7 + 1.5}s` 
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 1.5}s`; // Use backticks for the string to make it work.
+            } //console.log(link.style.animation);
         });
-
-        //BurgerIcon animation
-        burgerIcon.classList.toggle("toggle");
+        burgerIcon.classList.toggle("toggle"); //BurgerIcon animation
     });
-
-    
 } 
-
 
 navSlide();
