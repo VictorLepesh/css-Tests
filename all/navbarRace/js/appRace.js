fd2d530dcd09eceaae01e50b37bc6effe704d4b8
Vic <victorl175@protonmail.com>
2019-09-30 10:04:08 -0700

    Updating scores, practiced with CSS, no new high scores.  Almost got 20 points in appRace from the functino, just had a small typo

--- all/navbarRace/js/appRace.js
+++ all/navbarRace/js/appRace.js
@@ -1,1 +1,20 @@
-console.log("Test");
+const navSlide = () => {
+    const burgerIcon = document.querySelector(".burgerIcon");
+    const nav = document.querySelector(".nav-links");
+    const navLinks = document.querySelectorAll(".nav-links li");
+
+    burgerIcon.addEventListener("click", () => {
+        nav.classList.toggle("nav-active");
+
+        navLinks.forEach((link, index) => {
+            if (link.style.animatino) {
+                link.style.animation = "";
+            } else {
+                link.style.animatino = `navLinkFade 0.5s ease forwards ${index /7 + 1.5}s`;
+            }
+        });
+        burgerIcon.class.toggle("nav-active");
+    });
+}
+
+navSlide();
