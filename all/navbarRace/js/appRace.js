5e58cae1e76f0af13a915c4285006394a26f88d6
Victor Lepesh <victorl175@protonmail.com>
2019-10-02 18:37:53 -0700

    Updating scores, memorized the JS function, just have a few typos here and there.  As for CSS, my average is becoming 160-180 points

--- all/navbarRace/js/appRace.js
+++ all/navbarRace/js/appRace.js
@@ -1,16 +1,16 @@
 const navSlide = () => {
     const burgerIcon = document.querySelector(".burgerIcon");
     const nav = document.querySelector(".nav-links");
-    const navLinks = document.querySelectorAll(".nav-links li");
+    const navLinks = document.querySelectorAll(".nav-links a");
 
     burgerIcon.addEventListener("click", () => {
         nav.classList.toggle("nav-active");
 
-        navLinks.forEach((link, index) => {
+        navLinks.forEach((link, key) => {
             if (link.style.animation) {
                 link.style.animation = "";
             } else {
-                link.style.animation = `navLinkFade 0.5s forwards ease ${index / 7 + 1.5}s`
+                link.syle.animation = `navLinkFade 0.5s ease forwards ${key / 7 + 1.5}s`;
             }
         });
         burgerIcon.classList.toggle("toggle");
