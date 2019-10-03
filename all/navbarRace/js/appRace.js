de9025353a8ba505fab34fa3ba200183a757740d
Victor Lepesh <victorl175@protonmail.com>
2019-10-03 07:19:56 -0700

    Attempted to practice CSS lines passed line 54 made it about halfway today from memory a few times, everything else is reviewed

--- all/navbarRace/js/appRace.js
+++ all/navbarRace/js/appRace.js
@@ -1,20 +1,20 @@
 const navSlide = () => {
     const burgerIcon = document.querySelector(".burgerIcon");
     const nav = document.querySelector(".nav-links");
-    const navLinks = document.querySelectorAll(".nav-links a");
+    const navLinks = document.querySelectorAll(".nav-links li");
 
-    burgerIcon.addEventListener("click", () => {
+    burgerIcon.addEventListener("click", () => { // Toggle Nav
         nav.classList.toggle("nav-active");
 
-        navLinks.forEach((link, key) => {
+        navLinks.forEach((link, index) => { // Animate links
             if (link.style.animation) {
                 link.style.animation = "";
             } else {
-                link.syle.animation = `navLinkFade 0.5s ease forwards ${key / 7 + 1.5}s`;
-            }
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 1.5}s`; // Use backticks for the string to make it work.
+            } //console.log(link.style.animation);
         });
-        burgerIcon.classList.toggle("toggle");
+        burgerIcon.classList.toggle("toggle"); //BurgerIcon animation
     });
-}
+} 
 
 navSlide();
