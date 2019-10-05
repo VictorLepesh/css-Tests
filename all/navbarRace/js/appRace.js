372830a1b2d6d233c4459e450dfcf7b985c9e7fe
Victor Lepesh <victorl175@protonmail.com>
2019-10-04 20:30:49 -0700

    Learned a little about bringing methods up to access the app's main state and bringing props back down in React, reviewed Nav as usual.

--- all/navbarRace/js/appRace.js
+++ all/navbarRace/js/appRace.js
@@ -3,18 +3,18 @@ const navSlide = () => {
     const nav = document.querySelector(".nav-links");
     const navLinks = document.querySelectorAll(".nav-links li");
 
-    burgerIcon.addEventListener("click", () => { // Toggle Nav
+    burgerIcon.addEventListener("click", () => {
         nav.classList.toggle("nav-active");
 
-        navLinks.forEach((link, index) => { // Animate links
+        navLinks.forEach((link, key) => {
             if (link.style.animation) {
                 link.style.animation = "";
             } else {
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 1.5}s`; // Use backticks for the string to make it work.
-            } //console.log(link.style.animation);
+                link.style.animation = `navLinkFade 0.5s ease forwards ${key / 7 + 1.5}s`;
+            }
         });
-        burgerIcon.classList.toggle("toggle"); //BurgerIcon animation
+        burgerIcon.classList.toggle("toggle");
     });
-} 
+}
 
 navSlide();
