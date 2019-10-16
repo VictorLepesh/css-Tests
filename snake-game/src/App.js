099b6879a8b06919cf33c4dc85252238b3933ff2
Victor Lepesh <victorl175@protonmail.com>
2019-10-16 08:34:56 -0700

    Added some notes on locations of where I can see documentation and other similar bits of information.  Creating a Snake game with React

--- snake-game/src/App.js
+++ snake-game/src/App.js
@@ -1,12 +1,27 @@
-import React from 'react';
-import './App.css';
+import React, { Component } from 'react';
+import './index.css';
+import Snake from './Components/Snake/Snake';
+import Food from './Components/Food/Food';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends Component {
+  state = {
+    food: [6, 8],
+    snakeDots: [
+      [0, 0],
+      [2, 0]
+    ]
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="game-area">
+          <Snake snakeDots={this.state.snakeDots}/>
+          <Food dot={this.state.food}/>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
