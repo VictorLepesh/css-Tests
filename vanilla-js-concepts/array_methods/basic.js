8fb685dacae67eda75bda803e9ade625e9d3b4a9
Victor Lepesh <victorl175@protonmail.com>
2019-11-08 20:03:53 -0800

    Added notes on arrays.

---
+++ vanilla-js-concepts/array_methods/basic.js
@@ -0,0 +1,12 @@
+// Create an array
+
+let fruits = ['pear', 'apple'];
+
+// get length of array
+console.log(fruits.length);
+
+// Get last index of array
+console.log(fruits.length - 1);
+
+Array.prototype.customPropertyForAllArrays = "value";
+console.log(fruits.customPropertyForAllArrays);
