295d9daa9938c333562e0ae35842ce066a7f1736
VictorLepesh <victorl175@protonmail.com>
2021-01-22 15:35:34 -0800

    Added all properties starting with P

--- js/Object_Object/template/12.js
+++ js/Object_Object/template/12.js
@@ -1,8 +1,8 @@
 'use strict';
 
 
-obj.id = 2;
-obj.name = 'bob';
 Object.defineProperty(obj, 'id', { configurable: true, writable: true });
 console.log(Object.getOwnPropertyDescriptor(obj, 'id'));
+obj.id = 2;
+obj.name = 'bob';
 delete obj.id;
